86100a6ffb2d74e03dfbfec2d8c6c87fc6111c28
codewithirene567 <irenejinscott@gmail.com>
2020-07-23 23:52:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  #scope '/admin', module: 'admin' do #we're telling scope that we want to use /admin as a URL prefix
+  #  resources :stats, only: [:index] #letting rails know that all of our included routes are going to be handled by controlers in the admin module
+  #end
+  #anytime you want to route with a module and use that module's name as a URL prefix then use namespace
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
+
+#summary - 1)learned you can use scope to group all URLS by using an admin prefix
+#2) how to organize our controllers using directories and modules
+#3) how to use namespace to replace our scope, module
